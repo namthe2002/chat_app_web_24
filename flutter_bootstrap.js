5a82b13ead3fdedb55f53a31cef169f410659a4d
namthe2002 <80839052+namthe2002@users.noreply.github.com>
2024-08-09 14:15:56 +0700

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"0d93da7e2fe1a04e913ce44c0dc5daee3d8df5
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2619399324"
+    serviceWorkerVersion: "1297575899"
   }
 });
