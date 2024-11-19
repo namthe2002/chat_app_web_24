736c7619c623354fbbc8205d14eb5a78a2457835
PhamQuang2600 <100197752+PhamQuang2600@users.noreply.github.com>
2024-11-19 09:07:42 +0700

    update project

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3822200628"
+    serviceWorkerVersion: "1965744582"
   }
 });
